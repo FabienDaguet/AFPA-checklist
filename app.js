3f17d92b03d5134ad55b23f356ed09bb0f350e7f
Fabien <daguet77@gmail.com>
2020-10-27 09:31:12 +0100

    delete useless ligne and add comment for other possibilities

--- app.js
+++ app.js
@@ -3,69 +3,57 @@
 let input = document.forms.taskForm;
 let update = document.querySelector(".js-btn");
 let taskListe = document.querySelector(".js-list");
-let check = document.querySelectorAll(".form-check-input");
-let liste;
-let labelTask;
-let inputTask;
-let buttonTask;
+//let check = document.querySelectorAll(".form-check-input");
 let taskArray = [];
 
 function task(e) {
     e.preventDefault();
     let userTask = input.taskInput.value;
-    let addTask = taskArray.push(userTask);
+    taskArray.push(userTask);
     input.taskInput.value = "";
     console.log(taskArray)
+    updateTask();
 }
 
 function updateTask() {
     taskListe.innerHTML = "";
-    for (let i = 0; i < taskArray.length; i++) {
-        /*liste = document.createElement("li");
-        taskListe.appendChild(liste);
+    for (let i = 0; i < taskArray.length; i++) /* or for (task of taskArray) */{
+        let liste = document.createElement("li");
         liste.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
-        labelTask = document.createElement("label");
-        liste.appendChild(labelTask);
-        labelTask.classList.add("flex-grow-1");
-        inputTask = document.createElement("input");
-        inputTask.classList.add("form-check-input", "mr-1");
-        inputTask.setAttribute("type", "checkbox");
-        inputTask.addEventListener("input", checkin);
-        labelTask.appendChild(inputTask);
-        console.log(inputTask)
-        labelTask.innerHTML += taskArray[i];
-        buttonTask = document.createElement("button");
-        liste.appendChild(buttonTask);
-        buttonTask.setAttribute("type", "button");
-        buttonTask.classList.add("btn-close", "flex-shrink-0");*/
+        taskListe.appendChild(liste);
 
-        liste = document.createElement("li");
-        liste.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
-        taskListe.appendChild(liste);
-        labelTask = document.createElement("label");
+        let labelTask = document.createElement("label");
         labelTask.classList.add("flex-grow-1");
         liste.appendChild(labelTask);
-        inputTask = document.createElement("input");
+
+        let inputTask = document.createElement("input");
         inputTask.classList.add("form-check-input", "mr-1");
         inputTask.setAttribute("type", "checkbox");
         labelTask.appendChild(inputTask);
-        let text = document.createTextNode(taskArray[i]);
+
+        let text = document.createTextNode(taskArray[i] /* or task */);
         labelTask.appendChild(text);
+
         inputTask.addEventListener("input", checkin);
-        buttonTask = document.createElement("button");
+
+        let buttonTask = document.createElement("button");
         buttonTask.setAttribute("type", "button");
         buttonTask.classList.add("btn-close", "flex-shrink-0");
         liste.appendChild(buttonTask);
+
         buttonTask.addEventListener("click", close);
     }    
 }
 
 function checkin(event) {
     let target = event.currentTarget;
+    // target is not usefull
     if(target.checked) {
         target.parentElement.classList.add("text-decoration-line-through");
+        // or event.currentTarget.closest("li").classList.add("text-decoration-line-through");
    } else {
        target.parentElement.classList.remove("text-decoration-line-through");
+       //or event.currentTarget.closest("li").classList.remove("text-decoration-line-through");
    }
    console.log()
 }
@@ -76,4 +64,4 @@ function close(event) {
 }
 
 input.addEventListener("submit", task);
-input.addEventListener("submit", updateTask);
+update.addEventListener("click", updateTask);
